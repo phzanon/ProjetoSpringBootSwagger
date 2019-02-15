279247f371eaa0207c7079ea2482d59c85d114de
phzanon <p.hzanon123@gmail.com>
2019-02-15 17:23:31 -0200

    Deploy Maven Project

---
+++ src/main/java/com/ExemploSwagget/ExemploSwagger/ExemploSwaggerApplication.java
@@ -0,0 +1,14 @@
+package com.ExemploSwagget.ExemploSwagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploSwaggerApplication.class, args);
+	}
+
+}
+
